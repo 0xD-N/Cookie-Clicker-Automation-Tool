241256ff1fa6bc76d492ac83d0556852db5da989
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-03-04 01:54:27 -0600

    Major improvements
    Cleaned up code, removed unnecessary code, overall grew a brain.

--- autotyper/main.py
+++ autotyper/main.py
@@ -1,43 +1,83 @@
+from selenium.webdriver.opera.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import TimeoutException
 from Automation.Automation import Automation
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from threading import Thread
 import wget
 from bs4 import BeautifulSoup
 import time
 import os
 
+def getElement(driver: WebDriver, xPath, multiple=False, wait=10) -> WebElement:
+    try:
+        if(multiple):
+            code: WebElement = (WebDriverWait(driver, wait).until(lambda d: d.find_elements(By.XPATH, xPath)))
+            return code
+        
+        else:
+            code: WebElement = (WebDriverWait(driver, wait).until(lambda d: d.find_element(By.XPATH, xPath)))
+            return code
+            
+    except TimeoutException as t:
+        
+        driver.close()
+        time.sleep(1)
+        os.system("cls")
+        print(f"\nCouldn't find the speficied path: {xPath}")
+        driver.quit()
+        
+    except Exception as e:
+        
+        driver.close()
+        time.sleep(1)
+        os.system("cls")
+        print(f"\nAN ERROR HAS OCCURED")
+        print(f"\nLine {e.__traceback__.tb_lineno}: {e}")
+        driver.quit()
 
-
-if __name__ == "__main__":
+def start(clicksPerSecond = 1):
     
-    
-    SITE = "http://www.99lime.com/_bak/topics/html-is-easy/"
+    SITE = "https://orteil.dashnet.org/cookieclicker/"
     
     A = Automation().getDriver()
     
     soup = None
-    
-    images = []
-
-    with open("test.txt", "r") as f:
-    
-        images = f.readlines()
     
     try:
         
         A.get(SITE)
         
         soup = BeautifulSoup(f"{A.page_source}", "html.parser")
-        output: WebElement = (WebDriverWait(A, 10).until(lambda driver: driver.find_elements(By.TAG_NAME, "img")))
         
+        cookie = getElement(A, "(//*[@id=\"bigCookie\"])")
         
-        with open("test.txt", "w") as f:
+        output = getElement(A, "//*[@id=\"cookies\"]")
+        
+        cookie_amount = int(output.text[:output.text.index(" ")])
+        
+        products = getElement(A, "//*//span[contains(@id, \"productPrice\") and @class=\"price\"]//ancestor::div[contains(@class, \"product\")]", True)
+        
+        while(cookie_amount < 100000):
             
-            for i in range(len(output)):
-                f.write(str(output[i].get_attribute("src")+ "\n"))
-        
+            for i in range(clicksPerSecond):
+                cookie.click()
+            
+            cookie_amount = int(output.text[:output.text.index(" ")].replace("\n", "").replace(",", ""))
+            
+            for i in range(len(products)-1, -1, -1):
+               
+                if("unlocked" in products[i].get_attribute("class")):
+                    content_div = products[i].find_element(By.CLASS_NAME, "content")
+                    
+                    price = int(content_div.find_element(By.CLASS_NAME, "price").text.replace(",", "").split("\n")[0])
+                    
+                    if(cookie_amount >= price):
+                        products[i].click()
+                    
+            
         os.system("cls")
         A.quit()
         
@@ -49,6 +89,12 @@ if __name__ == "__main__":
         os.system("cls")
         print(f"\nAN ERROR HAS OCCURED")
         print(f"\nLine {e.__traceback__.tb_lineno}: {e}")
+    
+# //*//span[contains(@id, "productPrice") and @class="price"]//ancestor::div[contains(@class, "product")]     
+    
+if __name__ == "__main__":
+    start(2)
+    
 
 
 #in the div with dash-center class, it's contents are empty until the countdown of the game begins
